4732cc32e56ffd03ef3e456f31275b90bbc745b8
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-12-05 16:37:28 -0500

    initial commit

---
+++ auth_middleware.py
@@ -0,0 +1,25 @@
+from functools import wraps
+from flask import request, jsonify, g
+import jwt
+import os
+
+# auth_middleware.py
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        authorization_header = request.headers.get('Authorization')
+        # check if there is a header before attempting to decode it
+        if authorization_header is None:
+            return jsonify({"error": "Unauthorized"}), 401
+        try:
+            # remove the 'Bearer' portion of the Auth header string
+            token = authorization_header.split(' ')[1]
+            # decode will throw an error if the token is invalid, triggering the except block automatically
+            token_data = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=["HS256"])
+            g.user = token_data
+        except Exception as error:
+            return jsonify({"error": str(error)}), 500
+        
+        return f(*args, **kwargs)
+    return decorated_function
+
